3c00e22025ac17906b2c89dd06e2c4a766a7f949
BuddhaL1T3 <jmriggs75@gmail.com>
2018-08-06 19:03:49 -0400

    set stuff up :)

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,35 +1,36 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import requiresLogin from './requires-login';
-import {fetchProtectedData} from '../actions/protected-data';
+import { fetchQuestions } from '../actions/questions';
 
 export class Dashboard extends React.Component {
-    componentDidMount() {
-        this.props.dispatch(fetchProtectedData());
-    }
+  componentDidMount() {
+    this.props.dispatch(fetchQuestions());
+  }
 
-    render() {
-        return (
-            <div className="dashboard">
-                <div className="dashboard-username">
-                    Username: {this.props.username}
-                </div>
-                <div className="dashboard-name">Name: {this.props.name}</div>
-                <div className="dashboard-protected-data">
-                    Protected data: {this.props.protectedData}
-                </div>
-            </div>
-        );
-    }
+  render() {
+    console.log(this.props);
+    return (
+      <div className="dashboard">
+        <div className="dashboard-username">
+          Username: {this.props.username}
+        </div>
+        <div className="dashboard-name">Name: {this.props.name}</div>
+        <div className="dashboard-protected-data">
+          Protected data: {this.props.questions}
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    const {currentUser} = state.auth;
-    return {
-        username: state.auth.currentUser.username,
-        name: `${currentUser.firstName} ${currentUser.lastName}`,
-        protectedData: state.protectedData.data
-    };
+  const { currentUser } = state.auth;
+  return {
+    username: state.auth.currentUser.username,
+    name: `${currentUser.firstName} ${currentUser.lastName}`,
+    questions: state.questions.data
+  };
 };
 
 export default requiresLogin()(connect(mapStateToProps)(Dashboard));
